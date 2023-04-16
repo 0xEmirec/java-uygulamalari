275e395786fbc27246b0825620f59ed0c88b8ada
Recep Emir Yardım <49344583+0xEmirec@users.noreply.github.com>
2023-04-16 14:37:05 +0300

    Add files via upload

---
+++ KDVHesap.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class KDVHesap {
+    static Scanner klavye = new Scanner(System.in);
+    public static void main(String[] args) {
+        /*
+        KULLANICIDAN ALINAN FİYATIN KDV'SİNİ HESAPLAYIP KULLANICININ GİRDİĞİ TUTARAK EKLEYIP EKRANA BASAN PROGRAM
+        KDV ORANI = 18/100
+        */
+        System.out.println("Fiyat giriniz : ");
+        double fiyat = klavye.nextInt();
+        double kdv = (fiyat*18)/100;
+        double kdvFiyat = fiyat + kdv;
+        System.out.println("KDV'li fiyatınız : "+kdvFiyat);
+    }
+}
